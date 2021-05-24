a800e68d4c200fb1fc3d8ded9303820c321a3a12
kunalnarwani <56080406+kunalnarwani@users.noreply.github.com>
2021-05-24 12:21:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/xpressbeesapplication1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.xpressbeesapplication1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn1.setOnClickListener {
+            val page2Intent = Intent ( this, Page2::class.java)
+            startActivity(page2Intent)
+        }
+
+
+    }
+}
